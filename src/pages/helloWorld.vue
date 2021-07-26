8165e4cbc769f7b67a6b3af2c3701536ea63af18
朱原辰 <825913109@qq.com>
2021-07-26 18:02:18 +0800

    chore: 删除注释

--- src/pages/helloWorld.vue
+++ src/pages/helloWorld.vue
@@ -1,44 +1,5 @@
 <template>
   <div class="q-pa-md">
-    <!-- <q-carousel
-      swipeable
-      animated
-      arrows
-      v-model="slide"
-      v-model:fullscreen="fullscreen"
-      infinite
-    >
-      <q-carousel-slide
-        :name="1"
-        img-src="../image/lw1.jpg"
-      />
-      <q-carousel-slide
-        :name="2"
-        img-src="../image/lw2.jpg"
-      />
-      <q-carousel-slide
-        :name="3"
-        img-src="../image/lw3.jpg"
-      />
-      <q-carousel-slide
-        :name="4"
-        img-src="../image/lw4.jpg"
-      />
-
-      <template v-slot:control>
-        <q-carousel-control position="bottom-right" :offset="[18, 18]">
-          <q-btn
-            push
-            round
-            dense
-            color="white"
-            text-color="primary"
-            :icon="fullscreen ? 'fullscreen_exit' : 'fullscreen'"
-            @click="fullscreen = !fullscreen"
-          />
-        </q-carousel-control>
-      </template>
-    </q-carousel> -->
     <q-btn
       style="font-size: 30px"
       label="查看当前路由"
